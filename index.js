f56e1f2de6bcc086b9cda8a043871b61c87daa1e
Pedro Alves <3pedru@gmail.com>
2018-08-20 23:11:04 -0300

    adjusting basic configuration

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("teste")
+const express = require('express'),
+    app = express(),
+    helmet = require('helmet')
+
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.noCache())
+app.use(helmet.frameguard())
+
+app.get('/', (req, res) => {
+    res.status(200).json({ "abc": 123 })
+})
+
+app.post('/teste', (req, res) => {
+
+    res.status(200).json(req.body)
+})
+
+app.listen(3000)
