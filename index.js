ef2a58ce5729d4cfc712dcce5b90692c738a89d3
Pedro Alves <3pedru@gmail.com>
2018-08-20 23:55:25 -0300

    update

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const Telegraf = require('telegraf'),
-    bot = new Telegraf(process.env.TOKEN)
+    bot = new Telegraf(process.env.TOKEN),
+    http = require('http'),
+    port = process.env.PORT || 3000,
+    server = http.createServer((req, res) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Works');
+    });
 
 bot.start((ctx) => ctx.reply('Oi!'))
 bot.hears(['Oi', 'oi'], (ctx) => ctx.reply('tudo bem?'))
@@ -11,3 +18,7 @@ bot.command('modern', ({ reply }) => reply('Yo'))
 bot.command('hipster', Telegraf.reply('λ'))
 
 bot.startPolling()
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at ${port}`);
+});
